07b7a0738dce7584802612ccce0962f13c60be14
Joo Park <andypark6004@gmail.com>
2022-10-27 14:54:41 -0400

    Finsihing up framework
    Added set grid button
    Plan to enhance further

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const gridcontainer = document.querySelector("#gridContainer");
 let gridMax = 256;
 
-
+//Creates grids
 for (let x = 0; x < gridMax; x++){
     tempDiv = document.createElement("div");
     tempDiv.classList.add("grid");
     gridcontainer.appendChild(tempDiv);
 }
-
+//contain every grid into allGrids array
 allGrids = document.querySelectorAll(".grid");
 
+//set gridMax
+
+
+//function to replace class to grid hovered (plays hover animation)
 function playTransition(e){
     this.classList.replace("grid", "gridHovered");
 }
